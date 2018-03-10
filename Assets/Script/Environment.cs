fb4eb804ea2a8367daeecf76cd13b18d6f01ea3e
Agoraph0bia <jhoward132@gmail.com>
2018-03-10 15:50:13 -0800

    environment

---
+++ Assets/Script/Environment.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    [Range(0.0F, 100.0F)]
+    public static float Gravity = 35.0F;
+
+    private static WorldObject[] _WorldObjects;
+    public static WorldObject[] WorldObjects
+    {
+        get
+        {
+            return _WorldObjects;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+
+    private void Awake()
+    {
+        _WorldObjects = GetWorldObjects();
+    }
+
+    private WorldObject[] GetWorldObjects()
+    {
+        return Resources.FindObjectsOfTypeAll<WorldObject>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+		
+	}
+}
